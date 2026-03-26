bc45a8099e69c709be37afac0331de05d814497f
Damodar Barhate <barhatedamodar25@gmail.com>
2026-03-26 12:30:26 +0530

    Implemented Perceptron Using Gradient Descent

---
+++ 04_Perceptron_Algorithm_Classification/02_perceptron_using_stochastic.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+# Input features (4 samples, 2 features each)
+X = np.array([
+    [0, 0],
+    [0, 1],
+    [1, 0],
+    [1, 1]
+])
+
+# Target labels (OR logic)
+y = np.array([0, 1, 1, 1])
+
+def perceptron(x,y):
+    m = len(y)
+    learning_rate  = 0.5
+    iterations = 500
+    x_with_bias = np.column_stack((np.ones(shape=m),x))
+    n = x_with_bias.shape[1]
+    theta_j = np.zeros(shape=n)
+    for i in range(iterations):
+        idx = np.random.permutation(m)
+        x_with_bias = x_with_bias[idx]
+        y = y[idx]
+        for j in range(m):
+            x_i = x_with_bias[j]
+            y_i = y[j]
+            z = theta_j@x_i
+            # # its means that if z>0 then give 1 else give 0
+            y_predicted = np.where(z>0,1,0)
+            errors = y_i - y_predicted
+            gradient = errors * x_i
+            theta_j = theta_j + learning_rate*gradient
+    
+    return theta_j
+
+theta = perceptron(X,y)
+print("Intercept:",theta[0])
+print("Weights:",theta[1:])
+
+print("If we predict values based on the seen data x:")
+x_with_bias = np.column_stack((np.ones(shape=len(y)),X))
+z = theta@x_with_bias.T
+y_predict = np.where(z>0,1,0)
+print(y_predict)
